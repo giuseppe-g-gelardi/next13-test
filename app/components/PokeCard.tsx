ef3c570d1a2b454bdafff6143270446a84061885
giuseppe-g-gelardi <giuseppegelardi@icloud.com>
2022-12-02 19:21:04 -0500

    remove console.log displaying fetched information

--- app/components/PokeCard.tsx
+++ app/components/PokeCard.tsx
@@ -9,8 +9,6 @@ export default function PokeCard({ pokemon }: { pokemon: PokemonDetailResponse }
   const [showDetails, setShowDetails] = useState<boolean>(false);
   let [isOpen, setIsOpen] = useState(false)
 
-console.log(pokemon)
-
   return (
     <div className="relative h-80 mx-4">
       <button className='relative h-64 w-64 bg-slate-200 dark:bg-slate-600 rounded-[24px]' onClick={() => setIsOpen(true)}>
